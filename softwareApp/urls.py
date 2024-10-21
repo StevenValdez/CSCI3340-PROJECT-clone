c52a16459698a983854eda62541ce5cdb0e93658
Daniel <wemisiu@icloud.com>
2024-10-21 15:17:51 -0500

    Mon Oct 21

--- softwareApp/urls.py
+++ softwareApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = 'home'),    
+    path('',views.home, name = 'home'), 
+    #  path('login/',views.login_user, name = 'Login'),    
+    #   path('logout/',views.logout_user, name = 'Logout'),       
 ]
