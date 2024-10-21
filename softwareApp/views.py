5136a90b20b177add3141ee77555abfc0b1f8bc7
Daniel <wemisiu@icloud.com>
2024-10-21 15:18:55 -0500

    Updates

--- softwareApp/views.py
+++ softwareApp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
+
 
 def home(request):
     return render(request,'home.html',{})
+
+
+#deg login_user(request):
+
+# "" pass
+
+# def logout_user(request):
+ #   pass
+
+
+
 
 
